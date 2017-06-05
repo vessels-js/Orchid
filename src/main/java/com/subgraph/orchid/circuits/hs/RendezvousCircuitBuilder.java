e9a2f37226b226f75db8de04919703b697db8d90
George Carlson <gcarlson@adc4gis.com>
2017-06-05 14:34:55 -0500

    Update: Logging and Styling

--- src/main/java/com/subgraph/orchid/circuits/hs/RendezvousCircuitBuilder.java
+++ src/main/java/com/subgraph/orchid/circuits/hs/RendezvousCircuitBuilder.java
@@ -1,7 +1,6 @@
 package com.subgraph.orchid.circuits.hs;
 
 import java.util.concurrent.Callable;
-import java.util.logging.Logger;
 
 import com.subgraph.orchid.Circuit;
 import com.subgraph.orchid.Directory;
@@ -11,89 +10,91 @@ import com.subgraph.orchid.Router;
 import com.subgraph.orchid.TorException;
 import com.subgraph.orchid.circuits.CircuitManagerImpl;
 import com.subgraph.orchid.crypto.TorTapKeyAgreement;
+import com.subgraph.orchid.logging.Logger;
 
 public class RendezvousCircuitBuilder implements Callable<HiddenServiceCircuit>{
-	private final Logger logger = Logger.getLogger(RendezvousCircuitBuilder.class.getName());
+    private static final Logger logger = Logger.getInstance(RendezvousCircuitBuilder.class);
 	
-	private final Directory directory;
-	
-	private final CircuitManagerImpl circuitManager;
-	private final HiddenService hiddenService;
-	private final HSDescriptor serviceDescriptor;
-	
-	public RendezvousCircuitBuilder(Directory directory, CircuitManagerImpl circuitManager, HiddenService hiddenService, HSDescriptor descriptor) {
-		this.directory = directory;
-		this.circuitManager = circuitManager;
-		this.hiddenService = hiddenService;
-		this.serviceDescriptor = descriptor;
-	}
-	
-	public HiddenServiceCircuit call() throws Exception {
-		
-		logger.fine("Opening rendezvous circuit for "+ logServiceName());
-		
-		final InternalCircuit rendezvous = circuitManager.getCleanInternalCircuit();
-		logger.fine("Establishing rendezvous for "+ logServiceName());
-		RendezvousProcessor rp = new RendezvousProcessor(rendezvous);
-		if(!rp.establishRendezvous()) {
-			rendezvous.markForClose();
-			return null;
-		}
-		logger.fine("Opening introduction circuit for "+ logServiceName());
-		final IntroductionProcessor introductionProcessor = openIntroduction();
-		if(introductionProcessor == null) {
-			logger.info("Failed to open connection to any introduction point");
-			rendezvous.markForClose();
-			return null;
-		}
-		logger.fine("Sending introduce cell for "+ logServiceName());
-		final TorTapKeyAgreement kex = new TorTapKeyAgreement();
-		final boolean icResult = introductionProcessor.sendIntroduce(introductionProcessor.getServiceKey(), kex.getPublicKeyBytes(), rp.getCookie(), rp.getRendezvousRouter());
-		introductionProcessor.markCircuitForClose();
-		if(!icResult) {
-			rendezvous.markForClose();
-			return null;
-		}
-		logger.fine("Processing RV2 for "+ logServiceName());
-		HiddenServiceCircuit hsc = rp.processRendezvous2(kex);
-		if(hsc == null) {
-			rendezvous.markForClose();
-		}
+    private final Directory directory;
 
-		logger.fine("Rendezvous circuit opened for "+ logServiceName());
-		
-		return hsc;
-	}
+    private final CircuitManagerImpl circuitManager;
+    private final HiddenService hiddenService;
+    private final HSDescriptor serviceDescriptor;
+
+    public RendezvousCircuitBuilder(Directory directory, CircuitManagerImpl circuitManager, HiddenService hiddenService, HSDescriptor descriptor) {
+        this.directory = directory;
+        this.circuitManager = circuitManager;
+        this.hiddenService = hiddenService;
+        this.serviceDescriptor = descriptor;
+    }
 	
-	private String logServiceName() {
-		return hiddenService.getOnionAddressForLogging();
-	}
+    @Override
+    public HiddenServiceCircuit call() throws Exception {
+
+        logger.debug("Opening rendezvous circuit for "+ logServiceName());
+
+        final InternalCircuit rendezvous = circuitManager.getCleanInternalCircuit();
+        logger.debug("Establishing rendezvous for "+ logServiceName());
+        RendezvousProcessor rp = new RendezvousProcessor(rendezvous);
+        if(!rp.establishRendezvous()) {
+            rendezvous.markForClose();
+            return null;
+        }
+        logger.debug("Opening introduction circuit for "+ logServiceName());
+        final IntroductionProcessor introductionProcessor = openIntroduction();
+        if(introductionProcessor == null) {
+            logger.info("Failed to open connection to any introduction point");
+            rendezvous.markForClose();
+            return null;
+        }
+        logger.debug("Sending introduce cell for "+ logServiceName());
+        final TorTapKeyAgreement kex = new TorTapKeyAgreement();
+        final boolean icResult = introductionProcessor.sendIntroduce(introductionProcessor.getServiceKey(), kex.getPublicKeyBytes(), rp.getCookie(), rp.getRendezvousRouter());
+        introductionProcessor.markCircuitForClose();
+        if(!icResult) {
+            rendezvous.markForClose();
+            return null;
+        }
+        logger.debug("Processing RV2 for "+ logServiceName());
+        HiddenServiceCircuit hsc = rp.processRendezvous2(kex);
+        if(hsc == null) {
+            rendezvous.markForClose();
+        }
+
+        logger.debug("Rendezvous circuit opened for "+ logServiceName());
+
+        return hsc;
+    }
 	
-	private IntroductionProcessor openIntroduction() {
-		for(IntroductionPoint ip: serviceDescriptor.getShuffledIntroductionPoints()) {
-			final Circuit circuit = attemptOpenIntroductionCircuit(ip);
-			if(circuit != null) {
-				return new IntroductionProcessor(hiddenService, circuit, ip);
-			}
-		}
-		return null;
-	}
+    private String logServiceName() {
+        return hiddenService.getOnionAddressForLogging();
+    }
 	
-	private Circuit attemptOpenIntroductionCircuit(IntroductionPoint ip) {
-		final Router r = directory.getRouterByIdentity(ip.getIdentity());
-		if(r == null) {
-			return null;
-		}
-		
-		try {
-			final InternalCircuit circuit = circuitManager.getCleanInternalCircuit();
-			return circuit.cannibalizeToIntroductionPoint(r);
-		} catch (InterruptedException e) {
-			Thread.currentThread().interrupt();
-			return null;
-		} catch (TorException e) {
-			logger.fine("cannibalizeTo() failed : "+ e.getMessage());
-			return null;
-		}
-	}
+    private IntroductionProcessor openIntroduction() {
+        for(IntroductionPoint ip: serviceDescriptor.getShuffledIntroductionPoints()) {
+            final Circuit circuit = attemptOpenIntroductionCircuit(ip);
+            if(circuit != null) {
+                return new IntroductionProcessor(hiddenService, circuit, ip);
+            }
+        }
+        return null;
+    }
+	
+    private Circuit attemptOpenIntroductionCircuit(IntroductionPoint ip) {
+        final Router r = directory.getRouterByIdentity(ip.getIdentity());
+        if(r == null) {
+            return null;
+        }
+
+        try {
+            final InternalCircuit circuit = circuitManager.getCleanInternalCircuit();
+            return circuit.cannibalizeToIntroductionPoint(r);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return null;
+        } catch (TorException e) {
+            logger.debug("cannibalizeTo() failed : "+ e.getMessage());
+            return null;
+        }
+    }
 }
